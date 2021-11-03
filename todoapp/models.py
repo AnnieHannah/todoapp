03b98f5920ac4a2787f5ecf553d4c63b092f834c
Annie Hannah <annie@qburst.com>
2021-11-03 10:23:03 +0530

    Created model and serializer.

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+# Model for storing todo activities.
+class TodoModel(models.Model):
+    class IsCompleted(models.IntegerChoices):
+        completed = 1, 'Completed'
+        not_completed = 0, 'Not completed'
+
+    todo = models.CharField(max_length=500)
+    is_completed = models.IntegerField(choices=IsCompleted.choices, default=IsCompleted.not_completed)
+    expiry = models.DateTimeField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.todo
